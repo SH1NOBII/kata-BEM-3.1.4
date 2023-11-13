a3c7b967de9fd3d86c50a9a8c884a25f644b8f4f
Shinobi <igordp7777777@gmail.com>
2023-11-13 19:44:23 +0300

    fix media

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const moreButton = document.querySelector(".more-button");
 const contentText = document.querySelector('.content-text');
 const buttonText = document.querySelector('.more-button__text');
 const moreButtonIcon = document.querySelector('.icon-more-button');
+
+const contentTextHeight = contentText.style.height;
+
 let moreStatus = '';
 
 moreButton.addEventListener('click', function(){
     
+    
+
     if (moreStatus === '' || moreStatus === 'hidden') {
         contentText.style.height  = 'auto';
         buttonText.textContent = 'Скрыть';
@@ -15,7 +20,7 @@ moreButton.addEventListener('click', function(){
     } 
     
     if (moreStatus === 'opened'){
-        contentText.style.height  = '90px';
+        contentText.style.height  = contentTextHeight;
         buttonText.textContent = 'Читать далее';
         moreButtonIcon.style.transform = 'rotate(0)';
         return moreStatus = 'hidden';
